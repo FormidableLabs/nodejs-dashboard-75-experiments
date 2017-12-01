b8669fe9970786da57efc5537a9a6d4878925f34
Ryan Roemer <ryan@loose-bits.com>
2017-12-01 10:45:17 -0800

    Update env.js test and switch to fix branch

--- env.js
+++ env.js
@@ -2,7 +2,7 @@ console.log(`Node version: ${process.version}`);
 console.log(`Number of env vars: ${Object.keys(process.env).length}`);
 console.log("Env: node dashboard keys:");
 Object.keys(process.env)
-  .filter((k) => /nodejs-dashboard_/.test(k))
+  .filter((k) => /nodejs[-_]dashboard_/.test(k))
   .forEach((k) => {
     console.log(`${k}: ${process.env[k]}`);
   });
