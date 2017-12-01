91ae028ceae39e6a6ee5ec8db3bdd300439920e3
Ryan Roemer <ryan@loose-bits.com>
2017-11-30 16:38:34 -0800

    Add number of keys

--- env.js
+++ env.js
@@ -1,4 +1,5 @@
 console.log(`Node version: ${process.version}`);
+console.log(`Number of env vars: ${Object.keys(process.env).length}`);
 console.log("Env: node dashboard keys:");
 Object.keys(process.env)
   .filter((k) => /nodejs-dashboard_/.test(k))
